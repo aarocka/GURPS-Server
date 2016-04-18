95d469147dea86238003b8a4accb95e510d11e9d
Aaron Kolker <akolker3@gmail.com>
2016-04-18 17:14:53 -0400

    quasi working program

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-var express = require('express');
-var app = express();
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+users =[];
+connections =[];
 
-app.get('/createGame', function (req, res) {
-  //var userQueryString = req.query.message;
-  //res.send(userQueryString);
-  //create new row
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+io.on('connection', function(socket){
+  connections.push(socket);
+  console.log('Connected:' + connections.length + ' users connected');
+
+  socket.on('disconnect', function(data){
+    connections.splice(connections.indexOf(socket), 1);
+    console.log('Connected:' + connections.length + ' users connected');
+  });
+
+  socket.on('Send message', function(data){
+    io.sockets.emit('new message', {msg:data});
+  });
+
+  socket.on(123456, function(data){
+
+    console.log('Game 123456: Player ' + data);
+  });
+
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 });
