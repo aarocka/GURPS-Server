6ce5d797f1d6aff3fc45f66e40dc70da4a7ec777
Aaron Kolker <akolker3@gmail.com>
2016-04-27 16:46:18 -0400

    added join event

--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var mongodb = require('mongodb');
 users =[];
 connections =[];
+var GameInfo = {
+  "gameID": process.env.GAMEID || 123456,
+  "turn":1,
+  "players" :[]
+}
+var blankPlayer = {
+  "playerNumber":0,
+	"nicname":"",
+  "posX":0,
+  "posY":0,
+  "maxHealth":100,
+  "health":100
+};
 var port = process.env.HTTP_PORT || 3000;
+
+
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
-});
-
-app.get('/createRoom', function (req, res) {
-  //TODO Create new table to track room stats
-});
-
-app.get('/destroyRoom', function (req, res) {
-  //TODO Destroy table to track room stats
 });
 
 io.on('connection', function(socket){
@@ -26,20 +32,47 @@ io.on('connection', function(socket){
     connections.splice(connections.indexOf(socket), 1);
     console.log('Connected:' + connections.length + ' users connected');
   });
+  socket.on('join',function(data){
+    //Create player based on nicname provided
+    var tempPlayer = blankPlayer;
+    tempPlayer.nicname = data;
+
+    //assign player number
+    if (GameInfo.players.length == 0) {
+      tempPlayer.playerNumber=1
+    } else {
+      tempPlayer.playerNumber = GameInfo.players.length + 1;
+    }
+
+    //set player position based on player number
+    switch (tempPlayer.playerNumber) {
+      case 1:
+        tempPlayer.posY = 0;
+        tempPlayer.posX = 0;
+        break;
+      case 2:
+        tempPlayer.posY = 20;
+        tempPlayer.posX = 20;
+        break;
+      case 3:
+        tempPlayer.posY = 20;
+        tempPlayer.posX = 0;
+        break;
+      case 4:
+        tempPlayer.posY = 0;
+        tempPlayer.posX = 20;
+        break;
+      default:
+        tempPlayer.posY = 0;
+        tempPlayer.posX = 0;
+        break;
+    }
+    GameInfo.players.push(tempPlayer);
+  });
 
   socket.on(123456, function(data){
     console.log('Game 123456: Player: ' + data.playerNumber + ' GameState: ' + data.playerState);
     io.emit(123456, data);
-  });
-
-  socket.on(214358, function(data){
-    console.log('Game 123456: Player ' + data);
-    io.emit(214358, data);
-  });
-
-  socket.on(234567, function(data){
-    console.log('Game 123456: Player ' + data);
-    io.emit(234567, data);
   });
 
 });
